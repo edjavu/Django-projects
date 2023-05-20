bb2a4c0784e850000c677e28ab945d3b7d03f220
Eduardo Veintimilla <eduardojavy@gmail.com>
2023-05-20 15:51:16 -0500

    [UPD] Mostrar tareas propias del usuario, crear y actualizar tarea

--- django-crud-auth/tasks/views.py
+++ django-crud-auth/tasks/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
 
@@ -34,7 +35,8 @@ def signup(request):
                             })
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'tasks.html', {'tasks': tasks})
 
 def create_task(request):
     if request.method == 'GET':
@@ -42,10 +44,36 @@ def create_task(request):
             'form': TaskForm
         })
     else:
-        print(request.POST)
-        return render(request, 'create_task.html', {
-            'form': TaskForm
-        })
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+                'form': TaskForm,
+                'error': 'Please provide data validate'
+            })
+
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task': task, 'form': form})
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', {
+                'task': task, 
+                'form': form,
+                'error': "Error to updating task"
+                })
+
 
 
 def signout(request):

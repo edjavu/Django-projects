d43bd741aa217f307377ce16ba6f500b91fa482e
Eduardo Veintimilla <eduardojavy@gmail.com>
2023-05-22 20:58:53 -0500

    [UPD] Actualizar y borrar, estilos con bootstrap5 (terminado)

--- django-crud-auth/tasks/views.py
+++ django-crud-auth/tasks/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -34,10 +36,17 @@ def signup(request):
                                 'error': 'Password do not match'
                             })
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {'tasks': tasks})
 
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+@login_required
 def create_task(request):
     if request.method == 'GET':
         return render(request, 'create_task.html', {
@@ -56,6 +65,7 @@ def create_task(request):
                 'error': 'Please provide data validate'
             })
 
+@login_required
 def task_detail(request, task_id):
     if request.method == 'GET':
         task = get_object_or_404(Task, pk=task_id, user=request.user)
@@ -74,8 +84,22 @@ def task_detail(request, task_id):
                 'error': "Error to updating task"
                 })
 
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
 
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
 
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
